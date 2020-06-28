695144089a7f35e106be003c07e9754f1912c92e
陈文竹 <346852114@qq.com>
2020-06-28 10:50:20 +0800

    blog

--- src/pages/richText/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="richText">
-   <div style="margin:15px" v-html="value"></div>
-  </div>
-</template>
-<script src="./index.js"></script>
-<style lang="scss" scoped>
-@import "index.scss";
-</style>
